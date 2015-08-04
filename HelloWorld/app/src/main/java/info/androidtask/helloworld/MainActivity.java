c92f18e62620fd2cf43366a966f2adca9e633c91
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2015-08-04 13:51:27 -0300

    Hello World - Primeiro Projeto
    Criar primeiro projeto e entender sua estrutura.

---
+++ HelloWorld/app/src/main/java/info/androidtask/helloworld/MainActivity.java
@@ -0,0 +1,43 @@
+package info.androidtask.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /**
+         * Associando o arquivo de layout a nossa activity
+         */
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        /**
+         * Adicionando o menu a activity
+         */
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /**
+         * Método responsável por manipular os eventos de click do menu
+         */
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
